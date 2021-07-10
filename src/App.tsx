f8991224b8bdc522f9d0d36282d95367f5a15a2e
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-07-09 21:09:55 -0400

    feat: create basic scaffolding for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+// Components
+import QuestionCard from './components/QuestionCard'
 
-function App() {
-  return <div className="App">Quiz</div>
+const App = () => {
+  const startTrivia = () => {}
+
+  const nextQuestion = () => {}
+
+  return (
+    <div className="App">
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  )
 }
 
 export default App
