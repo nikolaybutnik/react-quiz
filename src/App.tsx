e986a1166ba47528e3fefc3bb248d38b93cd802f
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-07-09 21:36:15 -0400

    feat: create a basic question card and define props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 // Components
 import QuestionCard from './components/QuestionCard'
 
+const TOTAL_QUESTIONS = 10
+
 const App = () => {
+  const [number, setNumber] = useState(0)
+  const [questions, setQuestions] = useState([])
+  const [userAnswers, setUserAnswers] = useState([])
+
   const startTrivia = () => {}
 
   const nextQuestion = () => {}
+
+  const checkAnswer = () => {}
 
   return (
     <div className="App">
@@ -15,7 +23,14 @@ const App = () => {
       </button>
       <p className="score">Score:</p>
       <p>Loading Questions...</p>
-      <QuestionCard />
+      <QuestionCard
+        questionNumber={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number]}
+        answers={questions[number]}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
