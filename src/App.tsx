3f4672d9d28ed60882f593efb5519082a00fdeb1
Nikolay Butnik <65631838+mochi-dot-py@users.noreply.github.com>
2021-07-17 13:14:00 -0400

    style: add styling to header and score tracker

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import QuestionCard from './components/QuestionCard'
 // Types
 import { QuestionState, Difficulty } from './API'
 // Styles
-import { GlobalStyle } from './App.styles'
+import { GlobalStyle, Wrapper } from './App.styles'
 
 const TOTAL_QUESTIONS = 10
 
@@ -74,7 +74,7 @@ const App = () => {
   return (
     <>
       <GlobalStyle />
-      <div className="App">
+      <Wrapper>
         <h1>REACT QUIZ</h1>
         {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
           <button className="start" onClick={startTrivia}>
@@ -101,7 +101,7 @@ const App = () => {
             Next Question
           </button>
         ) : null}
-      </div>{' '}
+      </Wrapper>
     </>
   )
 }
